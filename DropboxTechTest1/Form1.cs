9933cf3c7ff07a9850b447ed7431c563461a753b
Andrey Rudenko <andrey.rudenko@gmail.com>
2017-05-09 22:16:28 +0300

    Draft server implementation, packages update.

--- DropboxTechTest1/Form1.cs
+++ DropboxTechTest1/Form1.cs
@@ -11,6 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Dropbox.Api;
+using ObjectDumper;
 using SimpleHttpServer;
 using SimpleHttpServer.Models;
 using SimpleHttpServer.RouteHandlers;
@@ -33,22 +34,9 @@ namespace DropboxTechTest1 {
       var routeConfig = new List<Route>() {
         new Route {
           Name = "Hello Handler",
-          UrlRegex = @"^/$",
+          UrlRegex = @"^/auth$",
           Method = "GET",
-          Callable = (HttpRequest request) => {
-            return new HttpResponse()
-            {
-              ContentAsUTF8 = "Hello from SimpleHttpServer",
-              ReasonPhrase = "OK",
-              StatusCode = "200"
-            };
-          }
-        },
-        new Route {
-            Name = "FileSystem Static Handler",
-            UrlRegex = @"^/Static/(.*)$",
-            Method = "GET",
-            Callable = new FileSystemRouteHandler() { BasePath = @"C:\Tmp", ShowDirectories=true }.Handle,
+          Callable = AuthAction,
         },
       };
 
@@ -58,13 +46,23 @@ namespace DropboxTechTest1 {
       var thread = new Thread(new ThreadStart(AppHttpServer.Listen));
       thread.Start();
 
+      var authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(
+        OAuthResponseType.Token,
+        SecretData.DropboxAppKey,
+        new Uri("http://localhost:48848/auth")
+      );
+      Process.Start(authorizeUri.ToString());
+    }
 
-//      var authorizeUri = DropboxOAuth2Helper.GetAuthorizeUri(
-//        OAuthResponseType.Token,
-//        SecretData.DropboxAppKey,
-//        new Uri("http://localhost:48848/auth")
-//      );
-//      Process.Start(authorizeUri.ToString());
+    private HttpResponse AuthAction(HttpRequest request) {
+      textBox1.AppendText(request + Environment.NewLine);
+//      textBox1.AppendText(request.DumpToString("request") + Environment.NewLine);
+      return new HttpResponse() {
+
+        ContentAsUTF8 = "Hello from SimpleHttpServer",
+        ReasonPhrase = "OK",
+        StatusCode = "200"
+      };
     }
   }
 }
